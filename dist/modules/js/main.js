234079de9b9b701a50a71a244f2747cb5fa18663
xiaoshanqiu <976784761@qq.com>
2017-08-27 20:22:13 +0800

    827

---
+++ dist/modules/js/main.js
@@ -0,0 +1,59 @@
+define('modules/js/main', function(require, exports, module) {
+
+  //index
+  "use strict";
+  
+  var index = {
+  	//banner轮播
+  	init: function init(num) {
+  		require(['modules/js/jquery'], function ($) {
+  			$(document).ready(function () {
+  				var picWidth = $(".bannerBox li img").width();
+  				var length = $(".bannerBox li").length;
+  				// alert(length)
+  				// console.log(picWidth)
+  				var i = 0;
+  				//函数
+  				function moveL() {
+  					i++;
+  					if (i == length) {
+  						i = 0;
+  					}
+  					$(".bannerBox").stop().animate({ left: -i * picWidth + "px" }, 500);
+  				}
+  				//左按钮
+  				$(".Larrow").click(function () {
+  					moveL();
+  				});
+  				//右按钮
+  				$(".Rarrow").click(function () {
+  					i--;
+  					if (i == -1) {
+  						i = length - 1;
+  					}
+  					$(".bannerBox").stop().animate({ left: -i * picWidth + "px" }, 1000);
+  				});
+  				//定时器
+  				var timer = setInterval(moveL, 2000);
+  
+  				$(".bannerBox").hover(function () {
+  					clearInterval(timer);
+  				}, function () {
+  					timer = setInterval(moveL, 2000);
+  				});
+  			});
+  		});
+  	}
+  
+  	/*show:function(num){
+   	require(['jquery'],function($){
+   		$(document).ready(function(){
+   			alert(1)
+   		})
+   	})
+   }*/
+  };
+  
+  exports.index = index;
+
+});
